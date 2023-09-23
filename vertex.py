606a8da11d862f5a8119b31033abf9f537735e79
maisafolgueral <maisa.folgueral@gmail.com>
2023-09-22 23:33:42 -0300

    Added vertex file

---
+++ vertex.py
@@ -0,0 +1,19 @@
+'''
+NOME - TIA
+Filipe Costa Pereira - 32106521
+Guilherme Guimarães Chiarelli - 32036647
+Maisa Folgueral - 32121385
+
+This file implements vertex class.
+It is used to store all vertex data.
+
+History
+Date: 2023-09-21
+Author: Filipe, Guilherme
+'''
+
+class Vertex:
+
+  def __init__(self, name, iso_code):
+    self.name = name # country name
+    self.iso_code = iso_code # ISO 3166 country codes
